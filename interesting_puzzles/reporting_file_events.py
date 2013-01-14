84ace08292dc7a549734cef151518412dafacff6
Antony Deepak Thomas <antonydeepak@gmail.com>
2013-01-14 17:35:15 -0600

    Update interesting_puzzles/reporting_file_events.py
    Updated solution for reporting file events

--- interesting_puzzles/reporting_file_events.py
+++ interesting_puzzles/reporting_file_events.py
@@ -28,49 +28,204 @@ Renamed dir /test -> /test2.'''
 # there are 2 kinds of hashes : contenthash & object hash
     # object hash - identifies the object
     # contenthash - identifies the constituents. for directory, it would be the hash of objects inside that directory
+    # dont store time
 class File:
-    def __init__(self,name,time,contenthash):
+    def __init__(self,name,parentname,time,contenthash):
         self.created_time = time
         self.modified_time = time
         self.name = name
+        self.parentname = parentname
         self.content_hash = contenthash
         self.status = "c"
-        self.events = ["c"]
-    def __eq__(self,file1,file2):
-        return True if (file1.content_hash == file2.content_hash) else False
+        self.events = [("c",time)]
     def delete(self,time):
         self.status = "d"
+        self.events.append(("d",self.time))
         self.modified_time = time
-        self.events.append("d")
-    def update(self,time):
+    def update(self,time,contenthash):
         self.status = "u"
+        self.events.append(("u",time,self.content_hash,contenthash))
+        self.content_hash = contenthash 
         self.modified_time = time
-        self.events.append("u")
-    def rename(self,time):
+    def rename(self,time,name):
         self.status = "r"
+        self.events.append(("r",time,self.name,name))
+        self.name = name 
         self.modified_time = time
-        self.events.append("r")
-    def move(self,time):
+    def move(self,time,parentname):
         self.status = "m"
+        self.events.append(("m",time,self.parentname,parentname))
+        self.parentname = parentname 
         self.modified_time = time
-        self.events.append("m")
     def __hash__(self):
-        return int(str(self.created_time) + str(name) 
+        return int(str(self.created_time) + str(name))
 
-class DropBox:
+'''class DropBox:
     def __init__(self):
         #making a strong assumption that .DP will not be used by any other file
         self._ft = {}
+        self._dels = []#tracks deletes at a transaction level
+        self._adds = []#tracks adds at a transaction level
 
     def add(self,name,time,contenthash):
+        if len(_adds) > 0 and _adds[0][0] != time:
+            process_lasttransaction()#getting into a new trans; so process last tran
+        self._adds.append((time,name,contenthash))
+        if len(_dels) > 0:#track only when there are deletes going on so this add could be a part of the different trans
+            self._adds.append((time,name,contenthash))
+        else:#just process it
+            object_name = get_object_name(name)
+            parent_name = get_parent_name(name)
+            newFile = File(object_name,parent_name,time,contenthash)
+            self._ft.add(object_name,newFile)
 
     def del(self,name,time,contenthash):
-        pass
+        if len(_dels) > 0 and _dels[0][0] != time:
+            process_lasttransaction()#getting into a new trans; so process last tran
+        self._dels.append((time,name,contenthash))
+
+
+    def _process_lasttransaction(self):
+        if(len(_dels) == 0):
+            return
+        if(len(_dels) == len(_adds) and _dels[0][0] == _adds[0]0]): #belong to a single transaction , either move,rename or update
+            affected_del = _dels[0]#you only care about the first affected object since this is part of the same transaction
+            affected_add = _adds[0]
+            if _is_move(affect_del,affected_add):
+                time,name,contenthash = affected_add
+                object_name = get_object_name(affected_del[1])
+                parentname = get_parent_name(name)
+                obj = self._ft[object_name]
+                obj.move(time,parentname)
+            elif _is_rename(affected_del,affected_add):
+                time,name,contenthash = affected_add
+                object_name = get_object_name(affected_del[1])
+                newname = get_object_name(name)
+                obj = self._ft[object_name]
+                obj.rename(time,newname)
+            elif _is_update(affected_del,affected_add):
+                time,name,contenthash = affected_add
+                object_name = get_object_name(affected_del[1])
+                obj = self._ft[object_name]
+                obj.update(time,contenthash)
+            else:
+                print 'Unfamilar condition that the program is not trained to handle'
+                raise
+        #individual delete actions; do not belong to a transaction.
+        for time,name,contenthash in _dels:
+            object_name = get_object_name(name)
+            obj = self._ft[object_name]
+            obj.delete(time)
+        #individual add actions; do not belong to a transaction.
+        for time,name,contenthash in _adds:
+            object_name = get_object_name(name)
+            parent_name = get_parent_name(name)
+            newFile = File(object_name,parent_name,time,contenthash)
+            self._ft.add(object_name,newFile)
+        self._dels = list()
+        self._adds = list()
+
+    def _is_move(del_action,add_action):
+        #move will result in a different parent but will have the same name & contenthash
+        oldname,_,oldcontenthash = del_action
+        newname,_,newcontenthash = add_action
+        old_parent_name = get_parent_name(oldname)
+        new_parent_name = get_parent_name(newname)
+        old_file_name = get_object_name(oldname)
+        new_file_name = get_object_name(newname)
+        return True if (old_parent_name != new_parent_name and old_file_name == new_file_name and oldcontenthash == newcontenthash) else False
+
+    def _is_rename(del_action,add_action):
+        #rename will result in a different name but will retain the parent & contenthash
+        oldname,_,oldcontenthash = del_action
+        newname,_,newcontenthash = add_action
+        old_parent_name = get_parent_name(oldname)
+        new_parent_name = get_parent_name(newname)
+        old_file_name = get_object_name(oldname)
+        new_file_name = get_object_name(newname)
+        return True if (old_parent_name == new_parent_name and old_file_name != new_file_name and oldcontenthash == newcontenthash) else False 
+
+    def _is_update(del_action,add_action):
+        #rename will result in a contenthash but will retain the parent & name
+        oldname,_,oldcontenthash = del_action
+        newname,_,newcontenthash = add_action
+        old_parent_name = get_parent_name(oldname)
+        new_parent_name = get_parent_name(newname)
+        old_file_name = get_object_name(oldname)
+        new_file_name = get_object_name(newname)
+        return True if (old_parent_name == new_parent_name and old_file_name == new_file_name and oldcontenthash != newcontenthash) else False'''
+
+
+
+
+def _can_combine_transactions(transA,transB):
+    """
+        Renames,moves & updates occur at different times but it can be combined into one transaction
+    """
+    if len(transA) != len(transB):
+        return false
+    for i in range(transA):
+        actionA,timeA,nameA,contenthashA = eventA = transA[0]
+        actionB,timeB,nameB,contenthashB = eventB = transB[0]
+        if(actionA != 'DEL' and actionB != 'ADD'):
+            return False 
+        if(int(timeB) - int(timeA) > 1):
+            return False #assuming that the timeinterval should be really short. if not, give the benefit of doubt as different transactions
+        isrename = _is_rename((filenameA,parentnameA,contenthashA),(filenameB,parentnameB,contenthashB))
+        isupdate = _is_update((filenameA,parentnameA,contenthashA),(filenameB,parentnameB,contenthashB))
+        ismove = _is_move((filenameA,parentnameA,contenthashA),(filenameB,parentnameB,contenthashB))
+        if(not(isrename or isupdate or ismove)):
+            return false
+    return true
+
+def _is_move(eventA,eventB):
+    filenameA,parentnameA,contenthashA = eventA
+    filenameB,parentnameB,contenthashB = eventB
+    return True if (parentnameA != parentnameB and filenameA == filenameB and contenthashA == contenthashB) else False
+
+def _is_update(eventA,eventB):
+    filenameA,parentnameA,contenthashA = eventA
+    filenameB,parentnameB,contenthashB = eventB
+     return True if (parentnameA == parentnameB and filenameA == filenameB and contenthashA != contenthashB) else False
+
+def _is_rename(eventA,eventB):
+    filenameA,parentnameA,contenthashA = eventA
+    filenameB,parentnameB,contenthashB = eventB
+    return True if (parentnameA == parentnameB and filenameA != filenameB and contenthashA == contenthashB) else False
+
+def get_parent_name(name):
+    pass
+
+def get_file_name(name):
+    pass
+
+
+def solution(events):
+    transactions = []
+    count = 0
+    prevtime = None
+    for event in events:
+        action,time,name,contenthash = event.split(' ')
+        if time == prevtime:
+            transactions[count-1].append((event))
+        else:
+            transactions.append([(event)])
+            count += 1
+        prevtime = time
+    while(i < len(transactions)-1):
+        if can_combine_transaction(transactions[i],transactions[i+1]):
+            combine(i,i+1)
+            i+=1
+        i+=1
+
+def test_sample_cases():
+    events = ['ADD 1282352346 /test -','ADD 1282353016 /test/1.txt f2fa762f','DEL 1282354012 /test -','DEL 1282354012 /test/1.txt f2fa762f','ADD 1282354013 /test2 -','ADD 1282354013 /test2/1.txt f2fa762f']
+    solution(events) 
 
 def test_file():
     dp_file = File(hash("test.txt"),"1282352346","-")
     assert dp_file.created_time == "1282352346"
-    assert dp_file.modified_time == "1282352346" 
+    assert dp_file.modified_time == "1282352346"
     assert dp_file.name == "test.txt"
     assert dp_file.content_hash == "-"
     assert dp_file.status == "created"
@@ -83,5 +238,5 @@ def test_file():
     print 'test_file tests pass'
 
 if __name__ == '__main__':
-    test_file()
-
+    #test_file()
+    test_sample_cases()
